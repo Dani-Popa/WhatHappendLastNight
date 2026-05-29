3665c6761c101fdc140ee6560105c299ebdfe815
Daniel Popa <daniel.popa@garmin.com>
2026-05-29 13:18:05 +0300

    Initial Commit

---
+++ WhatHappendLastNight/WhatHappendLastNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatHappendLastNightApp.swift
+//  WhatHappendLastNight
+//
+//  Created by Popa, Daniel on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhatHappendLastNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
